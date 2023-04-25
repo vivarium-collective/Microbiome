f9b9b1e9cf7473c7fccb67c010484f79f9791b50
Amin Boroomand <aboroomand@ucmerced.edu>
2023-04-25 13:11:32 -0400

    The code now includes the Reaction_bound_change function, which modifies the bounds at every time step. Currently, it decreases all bounds by 10% per time step, indicating the process of aging.

--- Models/dFBA_processes.py
+++ Models/dFBA_processes.py
@@ -4,7 +4,6 @@ import numpy as np
 from cobra.io import read_sbml_model
 from cobra.util import create_stoichiometric_matrix
 
-
 class DynamicFBA(Process):
     defaults = {}
 
@@ -18,6 +17,13 @@ class DynamicFBA(Process):
         for reaction in self.model.reactions:
             bounds[reaction.id] = (reaction.lower_bound, reaction.upper_bound)
         return bounds
+
+    def reaction_bound_change(self, percentage):  #This is an aging process which in each timestep we reduce 10% of flux bounds
+        for reaction in self.model.reactions:
+            old_bounds = self.bounds[reaction.id]
+            new_bounds = (old_bounds[0] * (1 - percentage), old_bounds[1] * (1 - percentage))
+            reaction.lower_bound, reaction.upper_bound = new_bounds
+            self.bounds[reaction.id] = new_bounds
 
     def ports_schema(self):
         return {
@@ -44,17 +50,24 @@ class DynamicFBA(Process):
         }
 
     def next_update(self, timestep, state):
+        # Use provided reaction_bounds for optimizing the model
+        reaction_bounds = state["reaction_bounds"]
+        for reaction_id, (lower_bound, upper_bound) in reaction_bounds.items():
+            reaction = self.model.reactions.get_by_id(reaction_id)
+            reaction.lower_bound, reaction.upper_bound = lower_bound, upper_bound
+
+        self.reaction_bound_change(0.1)  # Apply 10% reaction_bound_change
         solution = self.model.optimize()
         objective_value = solution.objective_value
         fluxes = solution.fluxes.to_dict()
         return {
             "fluxes": fluxes,
-            "objective_flux": objective_value
+            "objective_flux": objective_value,
+            "reaction_bounds": self.bounds
         }
 
 
-def main(model_path):
-    total_time = 1
+def main(model_path, simulation_time):
     parameters = {"model_file": model_path}
     dynamic_fba = DynamicFBA(parameters)
     processes = {'DynamicFBA': dynamic_fba}
@@ -63,11 +76,11 @@ def main(model_path):
             'fluxes': ("fluxes_values",),
             'reactions': ("reactions_list",),
             'objective_flux': ("objective_flux_value",),
-            'reaction_bounds': ("reaction_bounds",),
+            'reaction_bounds': ("reaction_bounds",),  # Connect the reaction_bounds output to the input
         }
     }
     sim = Engine(processes=processes, topology=topology)
-    sim.update(total_time)
+    sim.update(simulation_time)
     data = sim.emitter.get_data()
     output = pf(data)
     return output, processes, topology
