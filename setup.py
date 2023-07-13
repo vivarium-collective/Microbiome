98f9d7b4315243cdc9eeeaed83449d06ca68532a
Eran <agmon.eran@gmail.com>
2023-07-13 10:49:41 -0400

    change pytest path

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ with open("README.md", 'r') as readme:
     long_description = readme.read()
 
 setup(
-    name='vivarium-microbiome',  # TODO: Put your package name here.
+    name='vivarium-microbiome',
     version='0.0.1',
     packages=[
         'vivarium_microbiome',
         'vivarium_microbiome.processes',
     ],
     author='Amin Boroomand',
-    author_email='',  # TODO: Put your email here.
+    author_email='boroomand@uchc.edu',
     url='',  # TODO: Put your project URL here.
-    license='',  # TODO: Choose a license.
+    license='Apache 2',
     entry_points={
         'console_scripts': []},
-    short_description='',  # TODO: Describe your project briefely.
+    short_description='',  # TODO: Describe your project briefly.
     long_description=long_description,
     long_description_content_type='text/markdown',
     package_data={},
@@ -28,6 +28,5 @@ setup(
         'vivarium-core>=1.0.0',
         'pytest',
         'cobra',
-        # TODO: Add other dependencies.
     ],
 )
